43c8011a00653510b8c16821455673a68edd1a02
Alex Wilkinson <72150605+AlexWilkinsonnn@users.noreply.github.com>
2026-08-25 14:59:28 -0500

    flesh out optical hit monitoring alg

--- larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.cc
+++ larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.cc
@@ -1,7 +1,7 @@
 /**
  *  @file   larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.cc
  *
- *  @brief  Implementation of the particle visualisation algorithm.
+ *  @brief  Implementation of the optical hit monitoring algorithm.
  *
  *  $Log: $
  */
@@ -12,12 +12,22 @@
 
 #include "larpandoracontent/LArObjects/LArCaloHit.h"
 
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <map>
+
 using namespace pandora;
 
 namespace lar_content
 {
 
-OpHitMonitoringAlgorithm::OpHitMonitoringAlgorithm()
+OpHitMonitoringAlgorithm::OpHitMonitoringAlgorithm() :
+    m_opticalTimeExtent{50.f},
+    m_opticalMagnitudeScale{0.1f},
+    m_opticalTimeMin{0.f},
+    m_opticalTimeMax{20.f},
+    m_simpleMode{false}
 {
 }
 
@@ -33,24 +43,24 @@ StatusCode OpHitMonitoringAlgorithm::Run()
 {
     PANDORA_MONITORING_API(SetEveDisplayParameters(this->GetPandora(), true, DETECTOR_VIEW_XZ, -1, 1, 1));
 
-    const CaloHitList *pCaloHitList{nullptr};
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, "CaloHitListW", pCaloHitList));
+    for (const auto pfoListName : m_pfoListNames)
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->VisualizePfos(pfoListName));
+    }
 
     const CaloHitList *pOpHitList{nullptr};
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, "OpticalHitList", pOpHitList));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_opticalHitListName, pOpHitList));
 
-    PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &(*pCaloHitList), "Charge", GRAY));
+    std::map<unsigned int, CartesianVector> detectorPositions;
 
     for (const CaloHit *const pCaloHit : *pOpHitList)
     {
-        const LArOpHit *const pOpHit{dynamic_cast<const LArOpHit *>(pCaloHit)};
-        if (!pOpHit)
-            continue;
-        const CartesianVector pos{pOpHit->GetPositionVector()};
-        const float pe{pOpHit->GetInputEnergy() / 100.f};
-        const int peRound{static_cast<int>(std::ceil(pe))};
-        PANDORA_MONITORING_API(AddMarkerToVisualization(this->GetPandora(), &pos, "U true vertex", ORANGE, peRound));
-        std::cout << "OpHit(" << pos.GetX() << "," << pos.GetY() << "," << pos.GetZ() << "): " << pOpHit->GetInputEnergy() << std::endl;
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->VisualizeOpHit(pCaloHit, detectorPositions));
+    }
+
+    if (!m_simpleMode)
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->VisualizeOpDetTimeRanges(detectorPositions));
     }
 
     PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));
@@ -60,8 +70,109 @@ StatusCode OpHitMonitoringAlgorithm::Run()
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-StatusCode OpHitMonitoringAlgorithm::ReadSettings(const TiXmlHandle /*xmlHandle*/)
+StatusCode OpHitMonitoringAlgorithm::VisualizePfos(const std::string &listName) const
 {
+    const PfoList *pPfoList{nullptr};
+    if (STATUS_CODE_SUCCESS != PandoraContentApi::GetList(*this, listName, pPfoList))
+    {
+        if (PandoraContentApi::GetSettings(*this)->ShouldDisplayAlgorithmInfo())
+            std::cout << "OpHitMonitoringAlgorithm: pfo list " << listName << " unavailable." << std::endl;
+        return STATUS_CODE_SUCCESS;
+    }
+    PANDORA_MONITORING_API(VisualizeParticleFlowObjects(this->GetPandora(), pPfoList, listName.c_str(), AUTOITER, true, true));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode OpHitMonitoringAlgorithm::VisualizeOpHit(const CaloHit *const pCaloHit,
+    std::map<unsigned int, CartesianVector> &detectorPositions) const
+{
+    const LArOpHit *const pOpHit{dynamic_cast<const LArOpHit *>(pCaloHit)};
+    if (!pOpHit)
+        return STATUS_CODE_FAILURE;
+
+    const CartesianVector pos{pOpHit->GetPositionVector()};
+    const float magnitudeLength{pOpHit->GetInputEnergy() * m_opticalMagnitudeScale};
+
+    if (m_simpleMode)
+    {
+        const int markerSize{static_cast<int>(std::ceil(magnitudeLength))};
+        PANDORA_MONITORING_API(AddMarkerToVisualization(this->GetPandora(), &pos, "Optical Hit", ORANGE, markerSize));
+        return STATUS_CODE_SUCCESS;
+    }
+
+    const float peakTime{pOpHit->GetTime()};
+    const float displayTime{std::max(m_opticalTimeMin, std::min(peakTime, m_opticalTimeMax))};
+
+    const float halfWidth{0.5f * pOpHit->GetWidth()};
+    const float xStart{pos.GetX() - (0.5f * m_opticalTimeExtent) + (displayTime - m_opticalTimeMin - halfWidth) * m_opticalTimeScale};
+    const float xEnd{pos.GetX() - (0.5f * m_opticalTimeExtent) + (displayTime - m_opticalTimeMin + halfWidth) * m_opticalTimeScale};
+    const CartesianVector timeXStartPos(xStart, pos.GetY(), pos.GetZ());
+    const CartesianVector timeXEndPos(xEnd, pos.GetY(), pos.GetZ());
+
+    const CartesianVector peakPos((xStart + xEnd) * 0.5f, pos.GetY(), pos.GetZ());
+    const CartesianVector magnitudeYStartPos(peakPos.GetX(), peakPos.GetY() - magnitudeLength, peakPos.GetZ());
+    const CartesianVector magnitudeYEndPos(peakPos.GetX(), peakPos.GetY() + magnitudeLength, peakPos.GetZ());
+    const CartesianVector magnitudeZStartPos(peakPos.GetX(), peakPos.GetY(), peakPos.GetZ() - magnitudeLength);
+    const CartesianVector magnitudeZEndPos(peakPos.GetX(), peakPos.GetY(), peakPos.GetZ() + magnitudeLength);
+
+    const Color hitColor{(peakTime < m_opticalTimeMin) || (peakTime > m_opticalTimeMax) ? RED : ORANGE}; // If overflow/underflow time
+
+    detectorPositions.emplace(pOpHit->GetChannel(), pos);
+    PANDORA_MONITORING_API(AddLineToVisualization(this->GetPandora(), &timeXStartPos, &timeXEndPos, "OpHit time width", hitColor, 6, 1));
+    PANDORA_MONITORING_API(AddLineToVisualization(
+        this->GetPandora(), &magnitudeYStartPos, &magnitudeYEndPos, "OpHit magnitude Y", hitColor, 6, 1));
+    PANDORA_MONITORING_API(AddLineToVisualization(
+        this->GetPandora(), &magnitudeZStartPos, &magnitudeZEndPos, "OpHit magnitude Z", hitColor, 6, 1));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode OpHitMonitoringAlgorithm::VisualizeOpDetTimeRanges(const std::map<unsigned int, CartesianVector> &detectorPositions) const
+{
+    for (const auto &detectorEntry : detectorPositions)
+    {
+        const CartesianVector &pos{detectorEntry.second};
+        const CartesianVector startPos(pos.GetX() - (0.5f * m_opticalTimeExtent), pos.GetY(), pos.GetZ());
+        const CartesianVector endPos(pos.GetX() + (0.5f * m_opticalTimeExtent), pos.GetY(), pos.GetZ());
+        PANDORA_MONITORING_API(AddLineToVisualization(this->GetPandora(), &startPos, &endPos, "Optical detector time range", BLUE, 1, 1));
+    }
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode OpHitMonitoringAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OpticalHitListName", m_opticalHitListName));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF( STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "PfoListNames", m_pfoListNames));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "OpticalTimeExtent", m_opticalTimeExtent));
+    if (m_opticalTimeExtent <= 0.f)
+        return STATUS_CODE_INVALID_PARAMETER;
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "OpticalMagnitudeScale", m_opticalMagnitudeScale));
+    if (m_opticalMagnitudeScale <= 0.f)
+        return STATUS_CODE_INVALID_PARAMETER;
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "OpticalTimeMin", m_opticalTimeMin));
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "OpticalTimeMax", m_opticalTimeMax));
+    if (m_opticalTimeMax <= m_opticalTimeMin)
+        return STATUS_CODE_INVALID_PARAMETER;
+    m_opticalTimeScale = m_opticalTimeExtent / (m_opticalTimeMax - m_opticalTimeMin);
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "SimpleMode", m_simpleMode));
+
     return STATUS_CODE_SUCCESS;
 }
 
--- larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.h
+++ larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.h
@@ -1,7 +1,7 @@
 /**
  *  @file   larpandoracontent/LArMonitoring/OpHitMonitoringAlgorithm.h
  *
- *  @brief  Header file for the particle visualisation algorithm.
+ *  @brief  Header file for the optical hit monitoring algorithm.
  *
  *  $Log: $
  */
@@ -19,9 +19,9 @@ namespace lar_content
 class OpHitMonitoringAlgorithm : public pandora::Algorithm
 {
 public:
-    /**
-   *  @brief  Default constructor
-   */
+   /**
+    *  @brief  Default constructor
+    */
     OpHitMonitoringAlgorithm();
 
     virtual ~OpHitMonitoringAlgorithm();
@@ -29,6 +29,43 @@ public:
 private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    /**
+     *  @brief  Visualize a specified pfo list
+     *
+     *  @param[in]  listName the list name
+     */
+    pandora::StatusCode VisualizePfos(const std::string &listName) const;
+
+    /**
+     *  @brief  Visualize a single optical hit as glyph of 3 intersecting lines with x for hit width and yz for hit magnitude
+     *
+     *  @param[in]   pCaloHit the optical hit
+     *  @param[out]  detectorPositions map to store unique optical detector positions
+     */
+    pandora::StatusCode VisualizeOpHit(const pandora::CaloHit *const pCaloHit,
+        std::map<unsigned int, pandora::CartesianVector> &detectorPositions) const;
+
+    /**
+     *  @brief  Visualize the time axis of each optical detector
+     *
+     *  @param[in]  detectorPositions map with unique optical detector positions
+     */
+    pandora::StatusCode VisualizeOpDetTimeRanges(const std::map<unsigned int, pandora::CartesianVector> &detectorPositions) const;
+
+    /** Members set directly from the xml **/
+
+    std::string m_opticalHitListName;        ///< Name of optical hit list to visualize
+    std::vector<std::string> m_pfoListNames; ///< Names of pfo lists to visualize
+    float m_opticalTimeExtent;               ///< Extent in detector x coordinate of each optical detector's time axis [cm]
+    float m_opticalMagnitudeScale;           ///< Scale factor for converting hit magnitude into detector coordinates
+    float m_opticalTimeMin;                  ///< Minimum time of the optical detector time axes [us] (or [ns]?)
+    float m_opticalTimeMax;                  ///< Maximum time of the optical detector time axes [us] (or [ns]?)
+    bool m_simpleMode;                       ///< Flag to draw optical hits as markers at the optical detector xy, ignore time information
+
+    /** Other members **/
+
+    float m_opticalTimeScale; ///< Scale factor for converting hit times into detector coordinates on the optical detector time axes
 };
 
 } // namespace lar_content
